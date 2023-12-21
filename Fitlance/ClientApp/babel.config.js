5fa6a0631a7164257eede4cb4a7f2492048b253d
Thomas Friedrichs <friedrichs.tommy@gmail.com>
2023-12-21 14:09:48 -0800

    test pipeline

--- Fitlance/ClientApp/babel.config.js
+++ Fitlance/ClientApp/babel.config.js
@@ -11,7 +11,8 @@ const config = {
     "@babel/preset-react"
     ],
     "plugins": [
-        "@babel/plugin-proposal-private-property-in-object"
+        require('@babel/plugin-proposal-private-property-in-object').default,
+        require('@babel/plugin-proposal-private-methods').default
     ]
 }
 
