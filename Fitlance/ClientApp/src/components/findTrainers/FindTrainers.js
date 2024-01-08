6d7861be76b29b3e8f6e283930cb55defb889db4
Thomas Friedrichs <friedrichs.tommy@gmail.com>
2024-01-08 09:50:29 -0800

    Improve error handling in profile and find trainers pages

--- Fitlance/ClientApp/src/components/findTrainers/FindTrainers.js
+++ Fitlance/ClientApp/src/components/findTrainers/FindTrainers.js
@@ -9,6 +9,7 @@ import useLazyLoadTrainers from "./hooks/useLazyLoadTrainers";
 const FindTrainers = () => {
     const { displayedTrainers, isLoading, isError, error, loader } = useLazyLoadTrainers();
 
+
     if (isLoading) {
         return (
             <div className="fixed inset-0 bg-white bg-opacity-75 flex justify-center items-center z-50">
@@ -19,8 +20,24 @@ const FindTrainers = () => {
 
 
     if (isError) {
-        return <span className="mt-32">Error: {error.message}</span>
-    };
+        return (
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+                <div className="p-4 bg-white rounded-lg shadow-xl text-center">
+                    <h2 className="text-xl font-bold text-red-600">An Error Occurred</h2>
+                    <p className="text-red-600">{error.message}</p>
+                    <button
+                        onClick={() => window.location.reload()}
+                        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors"
+                    >
+                        Try Again
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+
+
 
     return (
         <div className="flex justify-center">
--- Fitlance/ClientApp/src/components/profile/Profile.js
+++ Fitlance/ClientApp/src/components/profile/Profile.js
@@ -27,8 +27,22 @@ const Profile = () => {
 
 
     if (isError) {
-        return <span className="mt-32">Error: {error.message}</span>
-    };
+        return (
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+                <div className="p-4 bg-white rounded-lg shadow-xl text-center">
+                    <h2 className="text-xl font-bold text-red-600">An Error Occurred</h2>
+                    <p className="text-red-600">{error.message}</p>
+                    <button
+                        onClick={() => window.location.reload()}
+                        className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition-colors"
+                    >
+                        Try Again
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
 
     return (
         <div className="flex justify-center">
